463db4882347956b897767335c4b396ff136f9fa
Philipp <mike.litoris@gmail.com>
2020-08-15 12:23:57 +0200

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,11 @@
+package com.androiddevs
+
+import io.ktor.application.Application
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+}
+
