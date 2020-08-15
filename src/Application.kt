8e491107e8cba4b3c68d9899feba432e8bb50f93
Philipp <mike.litoris@gmail.com>
2020-08-15 13:27:18 +0200

    Setup Authentication

--- src/Application.kt
+++ src/Application.kt
@@ -1,9 +1,13 @@
 package com.androiddevs
 
+import com.androiddevs.data.checkPasswordForEmail
 import com.androiddevs.routes.loginRoute
 import com.androiddevs.routes.registerRoute
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.UserIdPrincipal
+import io.ktor.auth.basic
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
@@ -26,5 +30,33 @@ fun Application.module(testing: Boolean = false) {
             setPrettyPrinting()
         }
     }
+    install(Authentication) {
+        configureAuth()
+    }
 }
+
+private fun Authentication.Configuration.configureAuth() {
+    basic {
+        realm = "Note Server"
+        validate { credentials ->
+            val email = credentials.name
+            val password = credentials.password
+            if(checkPasswordForEmail(email, password)) {
+                UserIdPrincipal(email)
+            } else null
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
