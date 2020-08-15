fe70e206bbbfebda18356f615c86285a77c6dca4
Philipp <mike.litoris@gmail.com>
2020-08-15 12:26:05 +0200

    The concept of Ktor

--- src/Application.kt
+++ src/Application.kt
@@ -1,11 +1,25 @@
 package com.androiddevs
 
 import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
+import io.ktor.routing.Routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing)
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
 }
 
